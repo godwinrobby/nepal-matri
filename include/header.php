44673eb83f61459e7771bc88a5ca9a52b3dff3d8
Godwin Robby <godwinrobby@gmail.com>
2019-10-06 12:19:30 +0530

    Add files via upload

---
+++ include/header.php
@@ -0,0 +1,99 @@
+            <header id="header" class="header-effect-shrink header-dark-bottom-border header-dark-bottom-border-1" data-plugin-options="{'stickyEnabled': true, 'stickyEffect': 'shrink', 'stickyEnableOnBoxed': true, 'stickyEnableOnMobile': true, 'stickyChangeLogo': true, 'stickyStartAt': 120, 'stickyHeaderContainerHeight': 70}">
+				<div class="header-body border-color-primary header-body-bottom-border">
+					<div class="header-top header-top-default border-bottom-0">
+						<div class="container">
+							<div class="header-row py-2">
+								<div class="header-column justify-content-start">
+									<div class="header-row">
+										<nav class="header-nav-top">
+											<ul class="nav nav-pills text-uppercase text-2">
+												<li class="nav-item nav-item-anim-icon d-none d-md-block">
+													<a class="nav-link pl-0" href="about-us.html"><i class="fas fa-angle-right"></i> About Us</a>
+												</li>
+												<li class="nav-item nav-item-anim-icon d-none d-md-block">
+													<a class="nav-link" href="contact-us.html"><i class="fas fa-angle-right"></i> Contact Us</a>
+												</li>
+											</ul>
+										</nav>
+									</div>
+								</div>
+								<div class="header-column justify-content-end">
+									<div class="header-row">
+										<nav class="header-nav-top">
+											<ul class="nav nav-pills">
+												<li class="nav-item">
+													<a href="mailto:mail@domain.com"><i class="far fa-envelope text-4 text-color-primary" style="top: 1px;"></i> mail@domain.com</a>
+												</li>
+												<li class="nav-item">
+													<a href="tel:123-456-7890"><i class="fab fa-whatsapp text-4 text-color-primary" style="top: 0;"></i> 123-456-7890</a>
+												</li>
+											</ul>
+										</nav>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="header-container container">
+						<div class="header-row">
+							<div class="header-column">
+								<div class="header-row">
+									<div class="header-logo">
+										<a href="index.html">
+											<img alt="Porto" width="100" height="48" data-sticky-width="82" data-sticky-height="40" src="img/logo.png">
+										</a>
+									</div>
+								</div>
+							</div>
+							<div class="header-column justify-content-end">
+								<div class="header-row">
+									<div class="header-nav header-nav-links order-2 order-lg-1">
+										<div class="header-nav-main header-nav-main-square header-nav-main-effect-2 header-nav-main-sub-effect-1">
+											<nav class="collapse">
+												<ul class="nav nav-pills" id="mainNav">
+													<li class="dropdown"><a class="dropdown-item dropdown-toggle" href="#">My Profile</a></li>
+													<li class="dropdown"><a class="dropdown-item dropdown-toggle" href="#">Matches</a></li>
+													<li class="dropdown"><a class="dropdown-item dropdown-toggle" href="#">Search</a></li>
+													<li class="dropdown"><a class="dropdown-item dropdown-toggle" href="#">Requests</a></li>
+												</ul>
+											</nav>
+										</div>
+										<button class="btn header-btn-collapse-nav" data-toggle="collapse" data-target=".header-nav-main nav">
+											<i class="fas fa-bars"></i>
+										</button>
+									</div>
+									<div class="header-nav-features header-nav-features-no-border header-nav-features-lg-show-border order-1 order-lg-2">
+										<div class="header-nav-feature header-nav-features-user header-nav-features-user-logged d-inline-flex mx-2 pr-2" id="headerAccount">
+											<a href="#" class="header-nav-features-toggle">
+												<i class="far fa-user"></i> JOHN DOE
+											</a>
+											<div class="header-nav-features-dropdown header-nav-features-dropdown-mobile-fixed header-nav-features-dropdown-force-right" id="headerTopUserDropdown">
+												<div class="row">
+													<div class="col-8">
+														<p class="mb-0 pb-0 text-2 line-height-1 pt-1">Hello,</p>
+														<p><strong class="text-color-dark text-4">John Doe</strong></p>
+													</div>
+													<div class="col-4">
+														<div class="d-flex justify-content-end">
+															<img class="rounded-circle" width="40" height="40" alt="" src="img/avatars/avatar.jpg">
+														</div>
+													</div>
+												</div>
+												<div class="row">
+													<div class="col">
+														<ul class="nav nav-list-simple flex-column text-3">
+															<li class="nav-item"><a class="nav-link" href="#">My Profile</a></li>
+															<li class="nav-item"><a class="nav-link" href="#">My Orders</a></li>
+															<li class="nav-item"><a class="nav-link border-bottom-0" href="#">Log Out</a></li>
+														</ul>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</header>
